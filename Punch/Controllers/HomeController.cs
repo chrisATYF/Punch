304f1ca3b6be492456727311126aa2a9ecfb18f8
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2018-06-05 18:24:30 -0500

    Added Services to HomeController

--- Punch/Controllers/HomeController.cs
+++ Punch/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Punch.Models;
+using Punch.Services;
 
 namespace Punch.Controllers
 {
@@ -12,10 +13,12 @@ namespace Punch.Controllers
     public class HomeController : Controller
     {
         protected readonly ApplicationDbContext _context;
+        protected readonly ClockService _clockService;
         
         public HomeController()
         {
             _context = new ApplicationDbContext();
+            _clockService = new ClockService();
         }
 
         [Route("", Name = "SplashPage")]
@@ -25,7 +28,8 @@ namespace Punch.Controllers
         }
         
         [Authorize]
-        public ActionResult PunchClock()
+        [Route("PunchClock/{id}", Name = "PunchClock")]
+        public ActionResult PunchClock(string id)
         {
             
             
@@ -36,6 +40,7 @@ namespace Punch.Controllers
 
         [Authorize]
         [HttpPost]
+        [Route("PunchClock", Name = "PunchClockPost")]
         public ActionResult PunchClock(ClockViewModel model)
         {
             
